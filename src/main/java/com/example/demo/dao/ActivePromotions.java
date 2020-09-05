1d7da1b43d3359f73d64fdc43768f4e8cf5756a3
rahul <dev.rahulprasad@gmail.com>
2020-09-06 03:06:32 +0530

    ActivePromotions class will have the all active promotion information. it can give you all the active promo by single unit or list of unit

---
+++ src/main/java/com/example/demo/dao/ActivePromotions.java
@@ -0,0 +1,53 @@
+package com.example.demo.dao;
+
+import lombok.Getter;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+@Getter
+public final class ActivePromotions {
+
+    private final String promotionId;
+    private final Collection<Promotion> promotions;
+    private final int value;
+
+    private static final Set<ActivePromotions> ACTIVE_PROMOTIONS = Set.of(
+            new ActivePromotions("promo1", Set.of(new Promotion("A", 3)), 130),
+            new ActivePromotions("promo2", Set.of(new Promotion("B", 2)), 45),
+            new ActivePromotions("promo3", Set.of(new Promotion("C", 1), new Promotion("D", 1)), 30)
+    );
+
+    public ActivePromotions(String promotionId, Collection<Promotion> promotions, int value) {
+        this.promotionId = promotionId;
+        this.promotions = promotions;
+        this.value = value;
+    }
+
+    public final Set<ActivePromotions> getActivePromotions() {
+        return ACTIVE_PROMOTIONS;
+    }
+
+    public static final Collection<ActivePromotions> getPromotionByUnitNames(Collection<String> unitNames) {
+        return ACTIVE_PROMOTIONS.stream()
+                .filter(byAvailableUnits(unitNames))
+                .collect(Collectors.toSet());
+    }
+
+    public final static Optional<ActivePromotions> getPromotionByUnitName(String unitName, Collection<ActivePromotions> activePromotions) {
+        return activePromotions.stream()
+                .filter(byAvailableUnit(unitName))
+                .findFirst();
+    }
+
+    private static Predicate<ActivePromotions> byAvailableUnits(Collection<String> unitNames) {
+        return activePromo -> activePromo.promotions.stream().anyMatch(promo -> unitNames.contains(promo.getUnitName()));
+    }
+
+    private static Predicate<ActivePromotions> byAvailableUnit(String unitName) {
+        return activePromo -> activePromo.promotions.stream().anyMatch(promo -> promo.equals(unitName));
+    }
+}
