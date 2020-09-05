5c220d590de5634059ec710126aa5b2d49795e29
rahul <dev.rahulprasad@gmail.com>
2020-09-06 03:05:20 +0530

    Promotion class added to model the which unit going to have promotion by how many quantity

---
+++ src/main/java/com/example/demo/dao/Promotion.java
@@ -0,0 +1,12 @@
+package com.example.demo.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Promotion {
+
+    private final String unitName;
+    private final int quantity;
+}
